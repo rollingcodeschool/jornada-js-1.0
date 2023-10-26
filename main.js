97e6db1f8e117a4315aa2e141335cab66f38c541
Pablo Marino <pmarino2013@gmail.com>
2023-10-26 16:29:35 -0300

    Mejoras en el README

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
 console.log("Hola mundo");
+
+let url = "http://localhost:3000";
+
+//traer usuarios de la BD
+
+const getUsuarios = async () => {
+  const resp = await fetch(url + "/" + "usuarios");
+  const data = await resp.json();
+
+  return data;
+};
+
+//Traer las charlas
+const getCharlas = async () => {
+  const resp = await fetch(url + "/" + "charlas");
+  const data = await resp.json();
+
+  return data;
+};
+
+document.querySelector("#btn-user").addEventListener("click", () => {
+  getUsuarios().then((respuesta) => {
+    console.log(respuesta);
+  });
+});
+
+document.querySelector("#btn-talks").addEventListener("click", () => {
+  getCharlas().then((respuesta) => {
+    console.log(respuesta);
+  });
+});
