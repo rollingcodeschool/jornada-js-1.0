7238a910311c8eb71509849a7a7230e927a4c9db
priscilabgmoya <105519676+priscilabgmoya@users.noreply.github.com>
2023-10-28 12:23:11 -0300

    se agrega tabla de usuairos y eventos

---
+++ helpers/helpDB.js
@@ -0,0 +1,59 @@
+const event =  [
+    {
+        "id": 1,
+        "título": "Introducción a Python",
+        "descripción": "Una charla introductoria a Python, uno de los lenguajes de programación más populares.",
+        "hora": "10:00 AM",
+        "orador": "Juan Pérez",
+        "imagen":"",
+        "asistentes":[],
+        "estado": true
+      },
+      {
+        "id": 2,
+        "título": "Desarrollo Web Moderno",
+        "descripción": "Explorando las tecnologías clave en el desarrollo web actual.",
+        "hora": "11:30 AM",
+        "orador": "Luisa Gómez",
+        "imagen":"",
+        "asistentes":[],
+        "estado": true
+      },
+      {
+        "id": 3,
+        "título": "Programación Orientada a Objetos",
+        "descripción": "Los conceptos fundamentales de la programación orientada a objetos.",
+        "hora": "1:00 PM",
+        "orador": "Carlos Rodríguez",
+        "imagen":"",
+        "asistentes":[],
+        "estado": true
+      },
+      {
+        "id": 4,
+        "título": "Inteligencia Artificial y Machine Learning",
+        "descripción": "Una introducción a la IA y el aprendizaje automático en la programación.",
+        "hora": "2:30 PM",
+        "orador": "Ana Martínez",
+        "imagen":"",
+        "asistentes":[],
+        "estado": true
+      },
+      {
+        "id": 5,
+        "título": "Optimización de Código",
+        "descripción": "Cómo mejorar el rendimiento y la eficiencia del código fuente.",
+        "hora": "4:00 PM",
+        "orador": "David López",
+        "imagen":"",
+        "asistentes":[],
+        "estado": true
+      }
+]; 
+const users = [
+    { "id": 1, "nombre": "json-server", "password": "typicode","email":"usuario@user.com", "estado": true , "rol": "ADMIN", "avatar":"https://w7.pngwing.com/pngs/254/985/png-transparent-actor-avatar-people-person-profile-user-voice-user-avatar-icon.png" }
+]
+
+export {
+    event, users
+}
--- js/administrador.js
+++ js/administrador.js
@@ -1,9 +1,17 @@
-const container = document.querySelector(".container-administrador"); 
-function TitlePpal (text) {
-    const title = document.createElement("h3"); 
-    title.innerText = `Administador de ${text}`; 
-    title.classList = "text-center"
-    return title; 
-}
+import {TitlePpal , bodyUser, bodyEvent , createTable, loadEvents, loadUser} from '../helpers/administradorHelpers.js'; 
+const location = window.location.pathname; 
 
-container.appendChild(TitlePpal("Usuarios")) ; 
+if(location.includes("administrador.html")){
+    const containerEvent = document.querySelector(".container-administrador"); 
+    containerEvent.appendChild(TitlePpal("Eventos")); 
+    containerEvent.appendChild(createTable(bodyEvent, "TbodyEvents")); 
+
+    const tbody = document.getElementById("TbodyEvents"); 
+    loadEvents(tbody); 
+}else {
+    const containerUser = document.querySelector(".container-administrador-user");
+    containerUser.appendChild(TitlePpal("Usuarios")); 
+    containerUser.appendChild(createTable(bodyUser, "TbodyUser")); 
+    const tbody = document.getElementById("TbodyUser"); 
+    loadUser(tbody);     
+}
