abf0786613f2e78d973b56de4d9641f22828a36b
Bianca Deblinger <biandeb96@gmail.com>
2023-10-28 11:00:47 -0300

    agrego register

--- js/helpers/helpers.js
+++ js/helpers/helpers.js
@@ -1,3 +1,15 @@
+'use strict'
+
+import { obtenerContactosDeLS, ordenarLista } from "./utilsReg";
+
+
 export const generateRandomId = () => {
     return self.crypto.randomUUID();
     };
+
+export const agregarContactoALS = (nuevoContacto) => {
+const contactos = ordenarLista(obtenerContactosDeLS());
+contactos.push(nuevoContacto);
+localStorage.setItem('contactos',JSON.stringify(contactos));
+}
+
