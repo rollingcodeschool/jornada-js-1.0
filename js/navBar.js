21b18194182284673755d5af8531b12f75885ee3
FrancoGuevara <francoguevara93@gmail.com>
2023-10-28 14:01:13 -0300

    Botones login/register

--- js/navBar.js
+++ js/navBar.js
@@ -1,4 +1,5 @@
-
+const adminLogueado = JSON.parse(localStorage.getItem('adminLogueado')) || false;
+const usuarioLogueado = JSON.parse(localStorage.getItem('usuarioLogueado')) || false;
 
 const header = document.querySelector("header");
 
@@ -13,135 +14,55 @@ const navbar = () => {
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-      <li class="nav-item">
-                 <a class="nav-link active text-white" aria-current="page" href="../index.html">Inicio</a>
-              </li>
-                ${
-                  user
-                    ? `
+                ${adminLogueado || usuarioLogueado
+      ? `
                     <li class="nav-item">
-                      <a class="nav-link text-white" href="../index.html">${user.userName}</a>
+                      ${user}
                     </li>`
-                    : `
+      : `
                     <li class="nav-item">
-                      <a class="nav-link text-white" href="../pages/login.html">Iniciar Sesión</a>
+                      <button class="btn btn-primary w-md-100 mb-md-3" href="../pages/login.html">Iniciar Sesión</button>
                     </li>
                 `
-                }
-                ${
-                  user
-                    ? ""
-                    : `
+    }
+                ${adminLogueado || usuarioLogueado
+      ? ""
+      : `
                   <li class="nav-item">
-                    <a class="nav-link text-white" href="../pages/register.html">Regístrate</a>
+                    <button class="btn btn-danger ms-lg-3 w-md-100" href="../pages/register.html">Regístrate</button>
                   </li>
                 `
-                }
-                ${
-                  user?.role === "Admin"
-                    ? `
+    }
+                ${adminLogueado
+      ? `
                     <li class="nav-item">
                       <a class="nav-link text-white" href="../pages/administrador.html">Panel de Admin.</a>
                     </li>
                     `
-                    : ""
-                }
-                ${
-                  user?.role === "User"
-                    ? `
+      : ""
+    }
+                ${usuarioLogueado
+      ? `
                     <li class="nav-item">
                       <a class="nav-link text-white" href="../index.html"></a>
                     </li>
                     `
-                    : ""
-                }
-                ${
-                  user
-                    ? `
+      : ""
+    }
+                ${adminLogueado || usuarioLogueado
+      ? `
                     <li class="nav-item">
                       <button class="btn text-white justify-content-md-end" id='closeSession' onclick='localStorage.clear(), window.location.reload()'>Cerrar Sesión</button>
                     </li>
                     `
-                    : ""
-                }
+      : ""
+    }
      </ul>
         
     </div>
   </div>
 </nav>
 `);
- };
-//   return (header.innerHTML = `
-//   <nav class="navbar navbar-total navbar-expand-lg navbar-dark fixed-top fw-bold p-3">
-//   <div class="section-main">
-//         <div class="container-fluid d-flex justify-content-sta align-items-center">
-      
-//         </div>
-//       </div>
-//     <div class="container-fluid">
-//       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-//         <span class="navbar-toggler-icon"></span>
-//       </button>
-//       <label for="menu">
-//         <img src="../assets/RC - Studio - 2.png" alt="menu" class="logo-navbar">
-//       <label/>
-//       <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-//         <ul class="navbar-nav">
-//           <li class="nav-item">
-//             <a class="nav-link active text-white" aria-current="page" href="">Inicio</a>
-//           </li>
-//           ${
-//             user
-//               ? `
-//               <li class="nav-item">
-//                 <a class="nav-link text-white" href="">${user.userName}</a>
-//               </li>`
-//               : `
-//               <li class="nav-item">
-//                 <a class="nav-link text-white" href="">Login</a>
-//               </li>
-//           `
-//           }
-//           ${
-//             user
-//               ? ""
-//               : `
-//             <li class="nav-item">
-//               <a class="nav-link text-white" href="../pages/register.html">Register</a>
-//             </li>
-//           `
-//           }
-//           ${
-//             user?.role === "ADMIN"
-//               ? `
-//               <li class="nav-item">
-//                 <a class="nav-link text-white" href="../pages/administrador.html">Panel de Admin.</a>
-//               </li>
-//               `
-//               : ""
-//           }
-//           ${
-//             user?.role === "USER"
-//               ? `
-//               <li class="nav-item">
-//                 <a class="nav-link text-white" href="../index.html"></a>
-//               </li>
-//               `
-//               : ""
-//           }
-//           ${
-//             user
-//               ? `
-//               <li class="nav-item">
-//                 <button class="btn text-white justify-content-md-end" id='closeSession' onclick='localStorage.clear(), window.location.reload()'>CERRAR SESION</button>
-//               </li>
-//               `
-//               : ""
-//           }
-//         </ul>
-//       </div>
-//     </div>
-//   </nav>`);
-// };
+};
 
 export default navbar;
