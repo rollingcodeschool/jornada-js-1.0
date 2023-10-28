556fa8647f67d7b79adcb83ca513a1c4daf21a74
Sofia Haiquel <sofihaiquel@gmail.com>
2023-10-28 13:28:07 -0300

    modifico estilo del footer

--- js/footer.js
+++ js/footer.js
@@ -2,19 +2,19 @@ const footer = document.querySelector("footer");
 
 const myFooter = () => {
   const user = JSON.parse(localStorage.getItem("userLog")) || undefined;
-  return (footer.innerHTML = `<section class="container-fluid mt-5 footer bg-dark">
-  <article class="row justify-content-around" id="cuerpoFooter">
+  return (footer.innerHTML = `<section class="container-fluid mt-5 footer bg-dark p-3">
+  <div class="row justify-content-around" id="cuerpoFooter">
     <aside class="col-xl-3 col-sm-3 col-md-3">
-      <p class="h5 text-white">Rolling Conf</p>
+      <p class="h5 text-white text-center">Rolling Conf</p>
       <ul>
         <li>General Paz 576</li>
-        <li>Teléfono: + 38 1 5783030</li>
+        <li>Teléfono: + 381 5783030</li>
         <li>Email: rollingcodeschool@gmail.com</li>
       </ul>
     </aside>
    
-    <aside class="col-xl-3 col-sm-3 col-md-3">
-      <p class="h5 text-white">Legal</p>
+    <aside class="col-xl-3 col-sm-3 col-md-3 aside-footer">
+      <p class="h5 text-white text-center">Legal</p>
       <ul>
         <li><a href="#">Términos y condiciones</a></li>
         <li><a href="#">Política de privacidad</a></li>
@@ -22,7 +22,7 @@ const myFooter = () => {
       </ul>
     </aside>
     <aside class="col-xl-3 col-sm-3 col-md-3">
-      <p class="h5 text-white">Redes Sociales</p>
+      <p class="h5 text-white text-center">Redes Sociales</p>
       <ul>
         <li>
         <a href="https://www.facebook.com/RollingCodeSchool/?locale=es_LA" class="img-redessociales"><i class="fa-brands fa-square-facebook"></i>
@@ -35,10 +35,10 @@ const myFooter = () => {
         </li>
       </ul>
     </aside>
-  </article>
-  <article class="row text-white" id="copyrightFooter">
+  </div>
+  <div class="row text-white" id="copyrightFooter">
     <p class="h6 text-center">&copy; 2023 Todos los derechos reservados.</p>
-  </article>
+  </div>
 </section>`);
 };
 
